56a709cb0c574d15801afdb784f9598b474d61c8
Cristian Montes <cmontesmujica@gmail.com>
2021-11-05 11:08:44 -0600

    added ToolAPIContainer sketleton

---
+++ src/containers/ToolAPIContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class ToolAPIContainer extends Component{
+
+
+    render(){
+        return(
+            <h1>THE TOOL API</h1>
+        );
+
+
+    }
+}
