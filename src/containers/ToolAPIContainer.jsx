443a11502703158607ddc179d6c1218ad0d9e80e
Cristian Montes <cmontesmujica@gmail.com>
2021-11-05 12:49:17 -0600

    fetch request function added needs to be tested

--- src/containers/ToolAPIContainer.jsx
+++ src/containers/ToolAPIContainer.jsx
@@ -3,12 +3,24 @@ import FormVerb from "../components/app/controls/FormVerb";
 
 
 export default class ToolAPIContainer extends Component{
+    state={
+        loading:false,
+    }
+
+
+
+
 
     render(){
         const verbs =  ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
         return(
             <FormVerb verbs={verbs}/>
         );
+
+
+
+
+
 
 
     }
